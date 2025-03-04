f86c3dfc314d524693d113d70fd44749e70163b0
Xuorus <ronanreilcatindig@gmail.com>
2025-03-04 11:03:39 +0800

    Updates relation problem

--- backend/models/pdfModel.js
+++ backend/models/pdfModel.js
@@ -3,8 +3,8 @@ const pool = require('../config/db');
 const savePDF = async (fileName, fileData) => {
   const client = await pool.connect();
   try {
-    const query = "INSERT INTO pds (PDSfile) VALUES ($1, $2)";
-    const values = [PDSfile];
+    const query = "INSERT INTO pds (pdsID, PDSfile) VALUES ($1, $2)";
+    const values = [fileName, fileData];
     await client.query(query, values);
   } finally {
     client.release();
