9e2a521abb34115d4bfc7f0966358e6dab279583
Xuorus <ronanreilcatindig@gmail.com>
2025-03-18 13:44:53 +0800

    just merge it

--- backend/routes/scanRoutes.js
+++ backend/routes/scanRoutes.js
@@ -4,7 +4,7 @@ const scanController = require('../controllers/scanController');
 
 // Define scan routes
 router.post('/start-scan', scanController.startScan);
-router.post('/person', scanController.addPerson);
+//router.post('/person', scanController.addPerson);
 router.get('/status/:scanId', scanController.getScanStatus);
 router.get('/preview/:tempId', scanController.getPreview);
 
